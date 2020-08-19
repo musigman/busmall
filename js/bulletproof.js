1f7920b0c82fc11725fef6ab0ce4555819f1b5da
Keith Musig <musigman@gmail.com>
2020-08-18 17:21:47 -0700

    busmall2

--- js/bulletproof.js
+++ js/bulletproof.js
@@ -1,69 +1,51 @@
 'use strict';
+
+
 // Global Variables
 var parentElement = document.getElementById('products');
 var finalList = document.getElementById('ul');
 var productArray = [];
 var totalClicks = 0;
 var maxClicks = 25;
-var uniqueImageArray = [];
 
-
-function Product (source, name){
-  this.title = name
+function Product (name){
+  this.title = `${name}`.slice(0,-4);
   // slice function from StackOverflow: https://stackoverflow.com/questions/952924/javascript-chop-slice-trim-off-last-character-in-string
-
+  this.title = this.title.charAt(0).toUpperCase() + this.title.slice(1);
   // capitaliation code from: https://paulund.co.uk/how-to-capitalize-the-first-letter-of-a-string-in-javascript
   this.alt = `${name}`;
-  this.filepath = `../img/${source}`;
+  this.filepath = `../img/${name}`;
   this.clicks = 0;
   this.shown = 0;
   productArray.push(this);
 }
-new Product('bag.jpg', 'Bag');
-new Product('banana.jpg', 'Banana');
-new Product('bathroom.jpg', 'Bathroom');
-new Product('boots.jpg', 'Boots');
-new Product('breakfast.jpg', 'Breakfast');
-new Product('bubblegum.jpg', 'Bubblegum');
-new Product('chair.jpg', 'Chair');
-new Product('cthulhu.jpg', 'Cthulhu');
-new Product('dog-duck.jpg', 'Dog Duck');
-new Product('dragon.jpg', 'Dragon');
-new Product('pen.jpg', 'Pen');
-new Product('pet-sweep.jpg', 'Pet Sweep');
-new Product('scissors.jpg', 'Scissors');
-new Product('shark.jpg', 'Shark');
-new Product('sweep.png', 'Sweep');
-new Product('tauntaun.jpg', 'Tauntaun');
-new Product('unicorn.jpg', 'Unicorn');
-new Product('usb.gif', 'USB');
-new Product('water-can.jpg', 'Water Can');
-new Product('wine-glass.jpg', 'Wine Glass');
+
+new Product('bag.jpg');
+new Product('banana.jpg');
+new Product('bathroom.jpg');
+new Product('boots.jpg');
+new Product('breakfast.jpg');
+new Product('bubblegum.jpg');
+new Product('chair.jpg');
+new Product('cthulhu.jpg');
+new Product('dog-duck.jpg');
+new Product('dragon.jpg');
+new Product('pen.jpg');
+new Product('pet-sweep.jpg');
+new Product('scissors.jpg');
+new Product('shark.jpg');
+new Product('sweep.png');
+new Product('tauntaun.jpg');
+new Product('unicorn.jpg');
+new Product('usb.gif');
+new Product('water-can.jpg');
+new Product('wine-glass.jpg');
+
 
 function getRandomImage(){
-
-
   var randomIndex = getRandomNumber(productArray.length);
-
-  // [1, 2, 4, 8, 9, 19]
-  while(uniqueImageArray.includes(randomIndex)){
-    randomIndex = getRandomNumber(productArray.length);
-  }
-  // add the index to the end of the array
-  uniqueImageArray.push(randomIndex);
-  // remove the oldest index from the array - that would be the first index
-  if(uniqueImageArray.length > 6){
-    uniqueImageArray.shift();
-  }
-
   var chosenImage = productArray[randomIndex];
   chosenImage.shown++;
-
-  buildElements(chosenImage);
-
-}
-function buildElements(chosenImage){
-  //building the image
   var imageElement = document.createElement('img');
   imageElement.setAttribute('src', chosenImage.filepath);
   imageElement.width = 400;
@@ -71,34 +53,17 @@ function buildElements(chosenImage){
   // padding code from w3schools" http://webdevable.com/w3schools/jsref/prop_style_padding.html
   imageElement.setAttribute('alt', chosenImage.alt);
   imageElement.setAttribute('title', chosenImage.title);
-
-  // create an input
-  // needs to have a type ="radio"
-  // needs to have a value="alt"
-  var radioButton = document.createElement('input');
-  radioButton.setAttribute('type', 'radio');
-  radioButton.setAttribute('value', chosenImage.alt);
-
-  //<input type="radio" value=chosenImage.alt/>
-
-  parentElement.appendChild(radioButton);
   parentElement.appendChild(imageElement);
 }
 
 function getRandomNumber(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
-// var radioButton = document.createElement ('input');
-// radioButton.setAttribute('type', 'radio');
-// radioButton.setAttribute('value', 'chosenImage.alt');
-// parentElement.appendChild(radioButton);
-// parentElement.appendChild(imageElement);
 
 function handleClick(event){
-  totalClicks++;
+  parentElement.onclick= totalClicks++;
   console.log('The total clicks were: '+ totalClicks);
-  console.log('the event.target', event.target.value);
-  var alt = event.target.value;
+  var alt = event.target.alt;
 
   for(var i=0; i<productArray.length; i++){
     if(alt === productArray[i].alt){
@@ -106,11 +71,11 @@ function handleClick(event){
       productArray[i].shown++;
     }
   }
+
   parentElement.innerHTML = '';
   getRandomImage();
   getRandomImage();
   getRandomImage();
-
   if (totalClicks>=maxClicks) {
     parentElement.removeEventListener('click', handleClick);
     for (var j = 0; j < productArray.length; j++){
@@ -120,16 +85,13 @@ function handleClick(event){
     }
   }
 }
-parentElement.addEventListener('click', handleClick);
-getRandomImage();
-getRandomImage();
-getRandomImage();
+
 
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ['Bag', 'Banana', 'Bathroom', 'Boots', 'Breakfast', 'Bubblegum', 'Chair', 'Cthulhu', 'Dog Duck', 'Dragon', 'Pen', 'Pet Sweep', 'Scissors', 'Shark', 'Sweep', 'Tauntaun', 'Unicorn', 'USB', 'Water Can', 'Wine Glass'],
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
     datasets: [{
       label: '# of Votes',
       data: [12, 19, 3, 5, 2, 3],
@@ -162,3 +124,8 @@ var myChart = new Chart(ctx, {
     }
   }
 });
+
+parentElement.addEventListener('click', handleClick);
+getRandomImage();
+getRandomImage();
+getRandomImage();
